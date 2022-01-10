3c6e173a757efd71b79285a4d2e4e943b2d982a6
Orla <orladunlop2@gmail.com>
2022-01-10 16:11:11 +0000

    Added /cat page with border, Sinatra reloader added

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
   "Hello World"
 end
 
 get '/secret' do
-  "This is a secret!"
+  "This is a secret! Again!"
+end
+
+get '/cat' do
+  "<div style ='border: 3px dashed red'>
+  <img src='https://i.imgur.com/jFaSxym.png'>
+  </div>"
 end
