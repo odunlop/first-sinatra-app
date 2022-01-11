75831bd674f191d414333152d4647794722f66a1
Orla <orladunlop2@gmail.com>
2022-01-11 14:17:33 +0000

    Added named-cat page to can choose the name

--- app.rb
+++ app.rb
@@ -9,7 +9,12 @@ get '/secret' do
   "This is a secret! Again!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end
